6c5085e8ad2de58014991709b8cd6b98f76615cb
thomashanahan <thmsshanahan0815@gmail.com>
2021-01-10 16:00:46 +0700

    [feat] update Support page

--- src/pages/Support/index.vue
+++ src/pages/Support/index.vue
@@ -1,20 +1,22 @@
+
 <template>
   <div>
     <Navigation />
     <div id="tos">
-      <b-row>
-        <b-col md="3"></b-col>
-        <b-col md="6">
-          <b-card>
-            <h1 class="mb-0 p-4 tx-18 wt-400">Support</h1>
-            <p>
-              <a target="_blank" v-bind:href="`mailto:${SUPPORT_EMAIL}`">
-                Questions or Support email {{ SUPPORT_EMAIL }}
-              </a>
-            </p>
-          </b-card>
-        </b-col>
-      </b-row>
+      <b-container>
+        <b-row>
+          <b-col sm="12" offset-md="2" md="8" offset-lg="3" lg="6">
+            <b-card>
+              <h1 class="mb-0 p-4 tx-18 wt-400">Support</h1>
+              <p>
+                <a target="_blank" v-bind:href="`mailto:${SUPPORT_EMAIL}`">
+                  Questions or Support email {{ SUPPORT_EMAIL }}
+                </a>
+              </p>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
   </div>
 </template>
@@ -39,7 +41,7 @@ export default {
 
 <style lang="scss" scoped>
 #tos {
-  margin-top: 150px;
+  margin-top: 105px;
   width: 100%;
   text-align: center;
 }
