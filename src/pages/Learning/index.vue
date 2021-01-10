0e24ab8b9b098cbab16124ebbebe12cc172d793a
thomashanahan <thmsshanahan0815@gmail.com>
2021-01-10 15:40:56 +0700

    [feat] update learning page

--- src/pages/Learning/index.vue
+++ src/pages/Learning/index.vue
@@ -2,28 +2,26 @@
   <div>
     <Navigation />
     <div id="learning">
-      <b-row>
-        <b-col md="12">
-          <b-card>
-            <h1 class="mb-0 wt-400">Candidate Rank Learning</h1>
-            <hr />
-            <b-row>
-              <b-col md="5">
-                <p>
-                  Candidate rank is a specialized application for medical staff
-                  to screen and interview incoming doctors. Candidate Rank
-                  allows you to import ERAS of SF Match candidate data and
-                  coordiante vitrual interviews. Users can also create surveys
-                  to coincide with virtual meetings to score candidates.
-                </p>
-                <a class="btn btn-success" v-bind:href="LOGIN_PAGE.HREF">
-                  Get started
-                </a>
-              </b-col>
-            </b-row>
-          </b-card>
-        </b-col>
-      </b-row>
+      <b-container>
+        <b-row>
+          <b-col sm="12" offset-md="2" md="8" offset-lg="3" lg="6">
+            <b-card>
+              <h1 class="mb-0 wt-400">Candidate Rank Learning</h1>
+              <hr />
+              <p>
+                Candidate rank is a specialized application for medical staff to
+                screen and interview incoming doctors. Candidate Rank allows you
+                to import ERAS of SF Match candidate data and coordiante vitrual
+                interviews. Users can also create surveys to coincide with
+                virtual meetings to score candidates.
+              </p>
+              <a class="btn btn-success" v-bind:href="LOGIN_PAGE.HREF">
+                Get started
+              </a>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
   </div>
 </template>
@@ -49,6 +47,6 @@ export default {
 <style lang="scss" scoped>
 #learning {
   width: 100%;
-  margin: 75px 5px 5px 5px;
+  padding: 105px 5px 5px 5px;
 }
 </style>
